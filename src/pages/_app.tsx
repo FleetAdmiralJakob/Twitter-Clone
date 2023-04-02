bf7f7830d9816a82b13671ce936acfdcbaaef312
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-04-02 14:05:34 +0200

    - Added Clerk
    - Added Posts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
-import { type AppType } from "next/app";
+import { AppProps, type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
