47efb94b5004718bbcc80a972fefff9054c52ef8
FleetAdmiralJakob <jakob.roessner@outlook.de>
2023-04-09 19:00:50 +0200

    Changed the font sizing

--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -24,7 +24,7 @@ const PostView = (props: PostWithUser) => {
           <span>{`@${author.username!}`}</span>
           <span className="font-thin">{`· ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
